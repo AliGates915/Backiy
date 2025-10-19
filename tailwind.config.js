8dbbb852153b20c3208bce15b3502bb979b1a5be
shahmeerdevdex <128673394+AliGates915@users.noreply.github.com>
2025-10-20 00:37:20 +0500

    aaa

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-anonymous-default-export */
+// tailwind.config.js
+export default {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
